583bbae2881196535ae89fa9bb6dbeb17714f26f
Will Greenberg <willg@eff.org>
2021-07-29 21:56:01 -0700

    working!

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use wasm_bindgen::prelude::*;
+use rustfft::{FftPlanner, num_complex::Complex};
+
+const AUDACITY_MAX_FREQ: u32 = 8000;
 
 #[wasm_bindgen]
 pub struct Image {
@@ -11,9 +14,9 @@ fn rgb_to_luma(r: u8, g: u8, b: u8, a: u8) -> u8 {
     if a < 10 {
         return 255;
     }
-    let mut luma = r as f64 * 0.2126;
-    luma += g as f64 * 0.7152;
-    luma += b as f64 * 0.0722;
+    let mut luma = r as f32 * 0.2126;
+    luma += g as f32 * 0.7152;
+    luma += b as f32 * 0.0722;
     luma as u8
 }
 
@@ -82,6 +85,81 @@ impl Image {
         self.width *= 2;
         self.data = reflected;
     }
+
+    fn to_complex_rows(&self) -> Vec<Vec<Complex<f32>>> {
+        let mut rows = Vec::new();
+        for i in 0..self.height {
+            let mut row = Vec::new();
+            for j in 0..self.width {
+                let idx = i * self.width + j;
+                row.push((self.data[idx] as f32).into());
+            }
+            rows.push(row);
+        }
+        rows
+    }
+}
+
+#[wasm_bindgen]
+pub struct Audio {
+    samples: Vec<f32>,
+    pub sample_rate: usize,
+}
+
+#[wasm_bindgen]
+impl Audio {
+    pub fn samples(&self) -> Vec<f32> {
+        self.samples.clone()
+    }
+
+    pub fn normalize(&mut self) {
+        let max = *self.samples.iter().max_by(|x, y| x.abs().partial_cmp(&y.abs()).unwrap()).unwrap();
+        self.samples.iter_mut().for_each(|s| *s = *s/max);
+    }
+}
+
+#[wasm_bindgen]
+pub fn image_to_audio(data: Vec<u8>, width: usize, height: usize, sample_rate: usize) -> Audio {
+    let mut img = Image::new(data, width, height);
+
+    // invert so that darker pixels result in high values
+    img.invert();
+
+    // zero-pad the top of the image so it "fits" into Audacity's 8kHz default spectrogram view
+    let scaling_factor = sample_rate as f32 / (2.0 * AUDACITY_MAX_FREQ as f32);
+    let padded_height = (height as f32 * scaling_factor) as usize;
+    img.pad_top(padded_height - height);
+
+    // rotate 90 degrees clockwise, since our IFFT wants to operate on image cols 
+    img.rotate90();
+
+    // add a symmetric set of "frequencies" to the end to create something that results in a real-
+    // valued waveform
+    img.reflect_about_y_axis();
+
+    let mut planner = FftPlanner::new();
+    let fft = planner.plan_fft_forward(2 * padded_height);
+    let mut audio = Audio {
+        samples: Vec::new(),
+        sample_rate,
+    };
+
+    // for each row of data (i.e. column of the image), perform an FFT, and concat the resulting
+    // waveform to our result
+    for mut row in img.to_complex_rows() {
+        fft.process(&mut row);
+        // we only need the real components (complex should be zero)
+        audio.samples.extend(row.iter().map(|c| c.re));
+    }
+
+    // remove the DC bias
+    let avg = audio.samples.iter().sum::<f32>() / audio.samples.len() as f32;
+    audio.samples.iter_mut().for_each(|s| *s = *s - avg);
+
+    // normalize to [-1, 1]
+    audio.normalize();
+
+    audio
 }
 
 #[wasm_bindgen]
